17d3e5287c64d034a6f81d7344c29758f1e3122a
Minh Ngo <bbbbbb267@gmail.com>
2023-02-21 11:14:52 +0100

    hash router

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
